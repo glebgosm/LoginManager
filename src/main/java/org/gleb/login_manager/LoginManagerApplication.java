ca560df2fabca05ee4950066a30a19efef016308
glebgosm <glebruler@mail.ru>
2021-06-04 15:49:32 +0300

    Switched to HTTPS protocol

--- src/main/java/org/gleb/login_manager/LoginManagerApplication.java
+++ src/main/java/org/gleb/login_manager/LoginManagerApplication.java
@@ -1,16 +1,54 @@
 package org.gleb.login_manager;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.gleb.login_manager.dao.UserDAOFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(UserDAOFactory.class)
 public class LoginManagerApplication {
 
+	@Value("${server.port}")
+	private int httpsPort;
+
 	public static void main(String[] args) {
 		SpringApplication.run(LoginManagerApplication.class, args);
+	}
+
+	// handle redirection from http to https
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(redirectConnector());
+		return tomcat;
+	}
+
+	private Connector redirectConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(httpsPort);
+		return connector;
 	}
 
 
