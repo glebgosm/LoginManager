9354ae65d4e271a8a0659243372208d1bbcf2c00
glebgosm <glebruler@mail.ru>
2021-06-07 21:35:49 +0300

    Set default HTTP (80) and HTTPS (443) ports + bug fix

--- src/main/java/org/gleb/login_manager/LoginManagerApplication.java
+++ src/main/java/org/gleb/login_manager/LoginManagerApplication.java
@@ -7,26 +7,28 @@ import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
-@PropertySource(value = "classpath:/application-${spring.profiles.active}" + ".properties", ignoreResourceNotFound = true)
+//@PropertySource(value = "classpath:/application-${spring.profiles.active}" + ".properties", ignoreResourceNotFound = true)
 public class LoginManagerApplication {
 
-	@Value("${server.port}")
+	@Value("${server.port:443}") // 443 is the default https port
 	private int httpsPort;
+
+	@Value("${spring.profiles.active}")
+	private String activeProfile;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginManagerApplication.class, args);
 	}
 
-	// handle redirection from http to https - only not in 'dev' mode
+	// handle redirection from http to https
 	@Bean
-	@ConditionalOnExpression("#{'${spring.profiles.active}' == 'https'}")
+	@Profile("https")
 	public ServletWebServerFactory servletContainer() {
 		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
 			@Override
@@ -46,7 +48,7 @@ public class LoginManagerApplication {
 	private Connector redirectConnector() {
 		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
 		connector.setScheme("http");
-		connector.setPort(8080);
+		connector.setPort(80);
 		connector.setSecure(false);
 		connector.setRedirectPort(httpsPort);
 		return connector;
