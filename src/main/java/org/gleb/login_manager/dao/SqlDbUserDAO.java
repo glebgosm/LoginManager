8cc88fde935bdcefea201569c941f4f394c51189
glebgosm <glebruler@mail.ru>
2021-06-07 19:15:56 +0300

    Refactored usage of PostgreSQL and H2 databases

--- src/main/java/org/gleb/login_manager/dao/SqlDbUserDAO.java
+++ src/main/java/org/gleb/login_manager/dao/SqlDbUserDAO.java
@@ -3,42 +3,34 @@ package org.gleb.login_manager.dao;
 import org.gleb.login_manager.model.User;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.sql.PreparedStatement;
-import java.util.List;
 
 @Component
-@ConditionalOnExpression("#{'${persistence.mode}'.equalsIgnoreCase('inmem_db')}")
-public class H2UserDAO implements UserDAO {
+@ConditionalOnExpression("#{'${persistence.mode}'.equalsIgnoreCase('sql_db')}")
+public class SqlDbUserDAO implements UserDAO {
 
     private UserRepository userRepository;
 
-    public H2UserDAO(UserRepository userRepository) {
+    public SqlDbUserDAO(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
-    @Transactional
     public User createUser(String userName, String password) {
         User user = new User(userName, password);
         return userRepository.save(user);
     }
 
     @Override
-    @Transactional
     public Iterable<User> getUsers() {
         return userRepository.findAll();
     }
 
     @Override
-    @Transactional
     public Iterable<User> getUserByName(String userName) {
         return userRepository.findByName(userName);
     }
 
     @Override
-    @Transactional
     public User getUserById(long id) {
         return userRepository.findById(id).orElse(null);
     }
