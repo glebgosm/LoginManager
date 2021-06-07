80775221e4a37df6077e4f8b236c6fafdfbf4dfa
glebgosm <glebruler@mail.ru>
2021-06-07 18:34:39 +0300

    Engaging HTTPS protocol in 'https' profile only

--- src/main/java/org/gleb/login_manager/LoginManagerApplication.java
+++ src/main/java/org/gleb/login_manager/LoginManagerApplication.java
@@ -8,13 +8,16 @@ import org.gleb.login_manager.dao.UserDAOFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @Import(UserDAOFactory.class)
+@PropertySource(value = "classpath:/application-${spring.profiles.active}" + ".properties", ignoreResourceNotFound = true)
 public class LoginManagerApplication {
 
 	@Value("${server.port}")
@@ -24,8 +27,9 @@ public class LoginManagerApplication {
 		SpringApplication.run(LoginManagerApplication.class, args);
 	}
 
-	// handle redirection from http to https
+	// handle redirection from http to https - only not in 'dev' mode
 	@Bean
+	@ConditionalOnExpression("#{'${spring.profiles.active}' == 'https'}")
 	public ServletWebServerFactory servletContainer() {
 		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
 			@Override
